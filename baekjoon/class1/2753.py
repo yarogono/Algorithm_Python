0aa3e9317df4e1c034d18b74a1fb86e0feb380ea
yarogono <naocya@naver.com>
2021-12-12 18:05:25 +0900

    baek 2753

---
+++ baekjoon/class1/2753.py
@@ -0,0 +1,9 @@
+year = input()
+        
+if 1 <= year and year <= 4000:
+    if year % 4 == 0 and year % 100 != 0:
+        print(1)
+    elif year % 4 == 0 and year % 400 == 0:
+        print(1)
+    else:
+        print(0)
